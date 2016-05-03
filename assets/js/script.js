ff25aff1b0d9af61f1d366139c0f491b3505f74e
Sergey Metlenkin <s.metlenkin@gmail.com>
2016-05-03 10:26:14 +0600

    Transfer to SemanticUI. Done with two stages.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,9 @@
-// window.addEvent('domready', function() {
-
-//   var test = new Fx.Accordion($$('h1'), $$('.accordion-el'), {
-//   });
-
-//   test.display(0);
-
-// });
-
 $(function () {
 
-    $('.accordion').accordion({
-        selector: {
-            trigger: '.title .icon'
-        }
+    $('.ui.accordion').accordion();
+
+    $('.special.cards .image').dimmer({
+        on: 'hover'
     });
 
 });
