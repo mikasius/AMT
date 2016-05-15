54c3dbfbacdd451167dcd04470172c7ec5a9213b
Sergey Metlenkin <s.metlenkin@gmail.com>
2016-05-15 10:41:44 +0600

    Начал работать над API. Добавил настройки VS Code.

---
+++ api/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function () {
+
+    var init = (function () {
+
+
+        new Vue({
+            el: '#app',
+            data: {
+                message: "Shit!"
+            }
+        });
+    })();
+
+
+});
+
+
